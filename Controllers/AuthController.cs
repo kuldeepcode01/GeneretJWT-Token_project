fb4e651e6756b900e5d1585efcf13592ce55f602
kuldeepcode01 <viratbhardwaj9670@gmail.com>
2024-10-17 11:04:26 +0530

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,74 @@
+﻿using JWT_Token.Bl.InterFace;
+using JWT_Token.Db.AppDbConnection;
+using JWT_Token.Db.Models;
+using JWT_Token.Db.Models.Api;
+using JWT_Token.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace GeneretJWT_Token.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+       
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
+        public AuthController(IConfiguration configuration,AppDbContext context)
+        {
+          
+            _configuration = configuration;
+            _context = context;
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestModel dto)
+        {
+            var chekUser = await _context.UserTable.FirstOrDefaultAsync(x => x.Email == dto.UserName && x.Password == dto.Password);
+            if (chekUser == null)
+            {
+                return Unauthorized();
+            }
+            var token = GeneretJwtToken(chekUser);
+            return Ok(new { token });
+
+        }
+        
+        [HttpGet("protected")]
+        [Authorize]
+        public IActionResult GetProtectedDat()
+        {
+            return Ok("This is a Protected endpoint");
+        }
+        private string GeneretJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name,user.Email),
+                    new Claim(ClaimTypes.Role,"User")
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials=new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256Signature),
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"],
+            };
+            var token=tokenHandler.CreateToken(tokenDescriptor);    
+            return tokenHandler.WriteToken(token);
+        }
+        
+
+
+
+            
+    }
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using JWT_Token.Bl.InterFace;
+using JWT_Token.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeneretJWT_Token.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployee _employee;
+
+        public EmployeeController(IEmployee Iemployee)
+        {
+            _employee = Iemployee;
+        }
+        [HttpGet("GetEmployee")]
+        public async Task<IActionResult>GetData()
+        {
+            var res = await _employee.GetEmployee();
+            return Ok(res);
+        }
+        [HttpGet("GetByEmployeeID")]
+        public async Task<IActionResult>GetByID(int id)
+        {
+            var res = await _employee.GetEmployeeByID(id);
+            return Ok(res);
+        }
+        [HttpPost("postData")]
+        public async Task<IActionResult>AddData(EmployeeDto employeeDto)
+        {
+            var res =await _employee.AddEmployee(employeeDto);
+            return Ok(res);
+        }
+        [HttpPut("UpdateData")]
+        public async Task<IActionResult>UpdateEmpData(EmployeeDto employeeDto)
+        {
+            var res =await _employee.UpdateEmployee(employeeDto);
+            return Ok(res);
+        }
+        [HttpDelete("Delete Data")]
+        public async Task<IActionResult>DeleteData(int id)
+        {
+            await _employee.DeleteEmployee(id);
+            return Ok("Data Deleted");
+        }
+    }
+}
